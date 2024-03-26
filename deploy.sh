e34f3069069e116f043bcf4cb96b122a37812b5f
Maximous ElKess Ayoub <89272370+maxiayoub@users.noreply.github.com>
2024-03-26 03:55:43 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=${{ vars.AWS_REGION }}
+export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
+export SSH_KEY_PATH=$SSH_KEY_PATH
+export EC2_PUBLIC_IP="44.211.144.45"
 
 # Copy application code to EC2 instance
 scp -i $SSH_KEY_PATH -r $APP_PATH ec2-user@$EC2_PUBLIC_IP:/home/python-app
