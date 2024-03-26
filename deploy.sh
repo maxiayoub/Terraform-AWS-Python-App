8868b51e94d2f0631d5e40181b37c5cb836a235f
max <max@localhost.localdomain>
2024-03-26 02:31:50 +0200

    first commit

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set AWS credentials
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=${{ vars.AWS_REGION }}
+
+# Copy application code to EC2 instance
+scp -i $SSH_KEY_PATH -r $APP_PATH ec2-user@$EC2_PUBLIC_IP:/home/python-app
+
+# SSH into EC2 instance and start application server
+ssh -i $SSH_KEY_PATH ec2-user@$EC2_PUBLIC_IP "cd /home/python-app && python app.py"
---
+++ main.tf
@@ -0,0 +1,49 @@
+# Creating VPC
+resource "aws_vpc" "demovpc" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Demo VPC"
+  }
+}
+
+# Creating Internet Gateway 
+resource "aws_internet_gateway" "demogateway" {
+  vpc_id = aws_vpc.demovpc.id
+}
+
+# Grant the internet access to VPC by updating its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.demovpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.demogateway.id
+}
+
+# Creating 1st subnet 
+resource "aws_subnet" "demosubnet1" {
+  vpc_id                  = aws_vpc.demovpc.id
+  cidr_block             = var.subnet1_cidr
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Demo subnet 1"
+  }
+}
+
+# Creating 2nd subnet 
+resource "aws_subnet" "demosubnet2" {
+  vpc_id                  = aws_vpc.demovpc.id
+  cidr_block             = var.subnet2_cidr
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Demo subnet 2"
+  }
+}
+
+
+# Creating EC2 instance in subnet 1
+resource "aws_instance" "example" {
+  ami           = "ami-0f403e3180720dd7e"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.demosubnet1.id
+}
