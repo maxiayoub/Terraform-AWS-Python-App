5d44fd2fe21d81696c577d901409277a3cc32bc9
Maximous ElKess Ayoub <89272370+maxiayoub@users.noreply.github.com>
2024-03-28 16:59:21 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+   region = "us-east-1"
+   key = "statefile02"
+   bucket = "terraform-aws-github-actions-max1"
+  }
+}
+
+
 provider "aws" {
   region = var.aws_region
 }
